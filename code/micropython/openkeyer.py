68a1662521c2c33e60599e761dd4bdd3ab7ba1a5
mdrisser <mdrisser@gmail.com>
2022-03-22 14:00:53 -0700

    Added more information to the README.md file

--- code/micropython/openkeyer.py
+++ code/micropython/openkeyer.py
@@ -1,55 +1,93 @@
-#####
-# IMPORTS
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  openkeyer.py
+#  
+#  Copyright 2022  <pi@rpi-003>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#
+import micropython
 import time
 
-from machine import Pin, I2C
+from machine import Pin, SoftI2C
 from ssd1306 import SSD1306_I2C
 
 
 #####
-# "CONSTANTS", well, as close as we can get
-# in Python
-OKVER = "0.1a"
-WIDTH = 128
-HEIGHT = 96
+# "CONSTANTS"
+OKVER = "0.1a"    # OpenKeyer Software Version
+IN2 = 17          # Input Pin #2 (DIT)
+IN1 = 16          # Input Pin #1 (DAH)
+OUT = 15          # Output Pin
+DIT = 275         # Length of time, in milliseconds, for a DIT
 
+DAH = DIT * 3     # A DAH is three times the length of a DIT in properly
+                  # timed morse code
 
 #####
-# GLOBAL VARIABLES
-sig_out = Pin(GP22, Pin.OUT)
-i2c = I2C(0)        # Initialize I2C using
-                    # the I2C0 defaults
-                    # SCL = Pin(GP9)
-                    # SDA = Pin(GP8)
-                    # freq = 400000
-oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
+# VARIABLES
+wpm = 0          # Words per Minute (Code Speed)
 
 
 #####
 # FUNCTIONS
-def initial_display():
-    # Initial text to display
-    oled.text("OPENKEYER V." + OKVER, 0, 0)
-    oled.text("BY: KG6ECW", 0, 10)
+def get_wpm(DIT, DAH):
+    intrachar = DIT
+    interchar = DAH
+    paris = DIT + intrachar + DAH + intrachar + DAH + intrachar + DIT + interchar
+    paris = paris + DIT + intrachar + DAH + interchar
+    paris = paris + DIT + intrachar + DAH + intrachar + DIT +interchar
+    paris = paris + DIT + intrachar + DIT + interchar
+    paris = paris + DIT + intrachar + DIT + intrachar + DIT
+    
+    speed = round(60000 / paris, 0)
+    return speed
+
+
+def initialDisplay(speed):
+    oled.text(f"OpenKeyer v.{OKVER}", 0, 0)
+    
+    oled.text("By: KG6ECW", 0, 10)
+    
+    oled.text("Initializing.....", 0, 20)
     oled.show()
     
-    # Wait 2 seconds
-    time.sleep_ms(2000)     # Using time.sleep_ms() avoids
-                            # possible incompatibilties
-                            # with other microcontollers
-    # Clear the display
-
-def main():
-    #|--REMOVE BEFORE FLIGHT-->
-    # Show the I2C info on the console
-    print("I2C Address: " + hex(i2c.scan()[0].upper()))
-    print(" I2C Config: " + src(i2c))
-    #<--REMOVE BEFORE FLIGHT--|
+    time.sleep_ms(2000)
     
-    initial_display()
+    oled.fill(0)
+    
+    oled.text(f"WPM: {speed}", 0, 0)
+    oled.show() 
 
 
 #####
-# RUNTIME
-if __name__ == "__main__":
-    main()
+# INITIALIZATION
+micropython.alloc_emergency_exception_buf(100)    # Error reporting for interupts
+
+dit = Pin(IN2, Pin.IN)
+dah = Pin(IN1, Pin.IN)
+out = Pin(OUT, Pin.OUT)
+
+i2c = SoftI2C(scl=Pin(1), sda=Pin(0), freq=100_000)
+i2c.scan()
+
+oled = SSD1306_I2C(128, 32, i2c)
+
+wpm = get_wpm(DIT, DAH)
+
+initialDisplay(wpm)
